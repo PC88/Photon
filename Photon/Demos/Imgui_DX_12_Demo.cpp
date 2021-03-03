93e7779a490a682f8a085e6584b100153f97137b
PC88 <32823980+PC88@users.noreply.github.com>
2021-03-03 19:17:21 +0000

    base functions added to the imgui test demo function.

--- Photon/Demos/Imgui_DX_12_Demo.cpp
+++ Photon/Demos/Imgui_DX_12_Demo.cpp
@@ -1,1 +1,26 @@
 #include "Imgui_DX_12_Demo.h"
+
+Imgui_DX_12_Demo::Imgui_DX_12_Demo()
+{
+
+}
+
+Imgui_DX_12_Demo::~Imgui_DX_12_Demo()
+{
+
+}
+
+void Imgui_DX_12_Demo::Update(double interval)
+{
+
+}
+
+void Imgui_DX_12_Demo::ImGuiRender()
+{
+
+}
+
+void Imgui_DX_12_Demo::Render()
+{
+
+}
--- Photon/Demos/Imgui_DX_12_Demo.h
+++ Photon/Demos/Imgui_DX_12_Demo.h
@@ -3,5 +3,13 @@
 class Imgui_DX_12_Demo :
     public Demo
 {
+
+	Imgui_DX_12_Demo();
+	virtual ~Imgui_DX_12_Demo();
+
+	// Optional inherited functions
+	virtual void Update(double interval) override;
+	virtual void ImGuiRender() override;
+	virtual void Render() override;
 };
 
