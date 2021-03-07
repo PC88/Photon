0d49793373d7bb232ed53c6a9d12fd827d21e590
PC88 <32823980+PC88@users.noreply.github.com>
2021-03-07 22:07:18 +0000

    vec3, refactored to device and host side functionality.

--- Photon/RT1W/vec3.h
+++ Photon/RT1W/vec3.h
@@ -19,18 +19,22 @@ class vec3
 public:
 
 public:
-	vec3() : e{ 0,0,0 } {}
-	vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
+	__host__ __device__ vec3() : e{ 0,0,0 } {}
+	__host__ __device__ vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
 
-	__host__ double x() const { return e[0]; }
-	double y() const { return e[1]; }
-	double z() const { return e[2]; }
+	// these were in-lined
+	__host__ __device__ double x() const { return e[0]; }
+	__host__ __device__ double y() const { return e[1]; }
+	__host__ __device__ double z() const { return e[2]; }
+	__host__ __device__ float r() const { return e[0]; }
+	__host__ __device__ float g() const { return e[1]; }
+	__host__ __device__ float b() const { return e[2]; }
 
-	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-	double operator[](int i) const { return e[i]; }
-	double& operator[](int i) { return e[i]; }
+	__host__ __device__ vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+	__host__ __device__ double operator[](int i) const { return e[i]; }
+	__host__ __device__ double& operator[](int i) { return e[i]; }
 
-	vec3& operator+=(const vec3 &v) 
+	__host__ __device__ vec3& operator+=(const vec3 &v)
 	{
 		e[0] += v.e[0];
 		e[1] += v.e[1];
@@ -38,7 +42,7 @@ public:
 		return *this;
 	}
 
-	vec3& operator*=(const double t) 
+	__host__ __device__ vec3& operator*=(const double t)
 	{
 		e[0] *= t;
 		e[1] *= t;
@@ -46,17 +50,17 @@ public:
 		return *this;
 	}
 
-	vec3& operator/=(const double t) 
+	__host__ __device__ vec3& operator/=(const double t)
 	{
 		return *this *= 1 / t;
 	}
 
-	double length() const 
+	__host__ __device__ double length() const
 	{
 		return sqrt(length_squared());
 	}
 
-	double length_squared() const 
+	__host__ __device__ double length_squared() const
 	{
 		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
 	}
@@ -88,51 +92,51 @@ inline std::ostream& operator<<(std::ostream &out, const vec3 &v)
 	return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
-inline vec3 operator+(const vec3 &u, const vec3 &v)
+__host__ __device__ inline vec3 operator+(const vec3 &u, const vec3 &v)
 {
 	return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
-inline vec3 operator-(const vec3 &u, const vec3 &v)
+__host__ __device__ inline vec3 operator-(const vec3 &u, const vec3 &v)
 {
 	return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &u, const vec3 &v)
+__host__ __device__ inline vec3 operator*(const vec3 &u, const vec3 &v)
 {
 	return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3 &v)
+__host__ __device__ inline vec3 operator*(double t, const vec3 &v)
 {
 	return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &v, double t)
+__host__ __device__ inline vec3 operator*(const vec3 &v, double t)
 {
 	return t * v;
 }
 
-inline vec3 operator/(vec3 v, double t)
+__host__ __device__ inline vec3 operator/(vec3 v, double t)
 {
 	return (1 / t) * v;
 }
 
-inline double dot(const vec3 &u, const vec3 &v)
+__host__ __device__ inline double dot(const vec3 &u, const vec3 &v)
 {
 	return u.e[0] * v.e[0]
 		+ u.e[1] * v.e[1]
 		+ u.e[2] * v.e[2];
 }
 
-inline vec3 cross(const vec3 &u, const vec3 &v)
+__host__ __device__ inline vec3 cross(const vec3 &u, const vec3 &v)
 {
 	return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
 		u.e[2] * v.e[0] - u.e[0] * v.e[2],
 		u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector(vec3 v)
+__host__ __device__ inline vec3 unit_vector(vec3 v)
 {
 	return v / v.length();
 }
