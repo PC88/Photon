3c3c28fa9acb32861b98a62abb42fd15f3714f7a
PC88 <32823980+PC88@users.noreply.github.com>
2021-03-07 18:03:52 +0000

    comments edited.

--- Photon/Source.cpp
+++ Photon/Source.cpp
@@ -67,17 +67,17 @@ static FrameContext                 g_frameContext[NUM_FRAMES_IN_FLIGHT] = {};
 static UINT                         g_frameIndex = 0;
 
 static int const                    NUM_BACK_BUFFERS = 3;
-static ID3D12Device* g_pd3dDevice = NULL;
-static ID3D12DescriptorHeap* g_pd3dRtvDescHeap = NULL;
-static ID3D12DescriptorHeap* g_pd3dSrvDescHeap = NULL;
-static ID3D12CommandQueue* g_pd3dCommandQueue = NULL;
-static ID3D12GraphicsCommandList* g_pd3dCommandList = NULL;
-static ID3D12Fence* g_fence = NULL;
+static ID3D12Device*				g_pd3dDevice = NULL;
+static ID3D12DescriptorHeap*		g_pd3dRtvDescHeap = NULL;
+static ID3D12DescriptorHeap*		g_pd3dSrvDescHeap = NULL;
+static ID3D12CommandQueue*			g_pd3dCommandQueue = NULL;
+static ID3D12GraphicsCommandList*	g_pd3dCommandList = NULL;
+static ID3D12Fence*					g_fence = NULL;
 static HANDLE                       g_fenceEvent = NULL;
 static UINT64                       g_fenceLastSignaledValue = 0;
-static IDXGISwapChain3* g_pSwapChain = NULL;
+static IDXGISwapChain3*				g_pSwapChain = NULL;
 static HANDLE                       g_hSwapChainWaitableObject = NULL;
-static ID3D12Resource* g_mainRenderTargetResource[NUM_BACK_BUFFERS] = {};
+static ID3D12Resource*				g_mainRenderTargetResource[NUM_BACK_BUFFERS] = {};
 static D3D12_CPU_DESCRIPTOR_HANDLE  g_mainRenderTargetDescriptor[NUM_BACK_BUFFERS] = {};
 
 // Forward declarations of helper functions
