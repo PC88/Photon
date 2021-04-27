43d802f32b4af11a363c2ca226983084a077fe58
PC88 <32823980+PC88@users.noreply.github.com>
2021-04-27 22:29:48 +0100

    GPU_RT Demo reduced to size,

--- Photon/Demos/GPU_RT_Demo.cu
+++ Photon/Demos/GPU_RT_Demo.cu
@@ -25,7 +25,7 @@ GPU_RT_Demo::GPU_RT_Demo()
 	// Image
 	auto aspect_ratio = 16.0 / 9.0;
 	int image_width = 400;
-	int samples_per_pixel = 10; // was 100
+	int samples_per_pixel = 2; // was 100
 	const int max_depth = 50;
 
 	// World
@@ -37,77 +37,14 @@ GPU_RT_Demo::GPU_RT_Demo()
 	auto aperture = 0.0;
 	color background(0, 0, 0);
 
-	switch (8)
-	{
-	case 1:
-		world = random_scene();
-		background = color(0.70, 0.80, 1.00);
-		lookfrom = point3(13, 2, 3);
-		lookat = point3(0, 0, 0);
-		vfov = 20.0;
-		aperture = 0.1;
-		break;
-
-	default:
-	case 2:
-		world = two_spheres();
-		background = color(0.70, 0.80, 1.00);
-		lookfrom = point3(13, 2, 3);
-		lookat = point3(0, 0, 0);
-		vfov = 20.0;
-		break;
-	case 3:
-		world = two_perlin_spheres();
-		background = color(0.70, 0.80, 1.00);
-		lookfrom = point3(13, 2, 3);
-		lookat = point3(0, 0, 0);
-		vfov = 20.0;
-		break;
-	case 4:
-		world = earth();
-		background = color(0.70, 0.80, 1.00);
-		lookfrom = point3(13, 2, 3);
-		lookat = point3(0, 0, 0);
-		vfov = 20.0;
-		break;
-	case 5:
-		world = simple_light();
-		samples_per_pixel = 400;
-		background = color(0, 0, 0);
-		lookfrom = point3(26, 3, 6);
-		lookat = point3(0, 2, 0);
-		vfov = 20.0;
-		break;
-	case 6:
-		world = cornell_box();
-		aspect_ratio = 1.0;
-		image_width = 600;
-		samples_per_pixel = 200;
-		background = color(0, 0, 0);
-		lookfrom = point3(278, 278, -800);
-		lookat = point3(278, 278, 0);
-		vfov = 40.0;
-		break;
-	case 7:
-		world = cornell_smoke();
-		aspect_ratio = 1.0;
-		image_width = 600;
-		samples_per_pixel = 200;
-		lookfrom = point3(278, 278, -800);
-		lookat = point3(278, 278, 0);
-		vfov = 40.0;
-		break;
-	case 8:
-		world = final_scene();
-		aspect_ratio = 1.0;
-		image_width = 800;
-		samples_per_pixel = 10;
-		background = color(0, 0, 0);
-		lookfrom = point3(478, 278, -600);
-		lookat = point3(278, 278, 0);
-		vfov = 40.0;
-		break;
-	}
+	world = final_scene();
+	aspect_ratio = 1.0;
+	image_width = 800;
+	samples_per_pixel = 2;
+	background = color(0, 0, 0);
+	lookfrom = point3(478, 278, -600);
+	lookat = point3(278, 278, 0);
+	vfov = 40.0;
 
 	// Camera
 	vec3 vup(0, 1, 0);
@@ -139,13 +76,11 @@ GPU_RT_Demo::GPU_RT_Demo()
 				ray r = cam.get_ray(u, v);
 				pixel_color += ray_color(r, background, world, max_depth);
 			}
-			//write_color(std::cout, pixel_color, samples_per_pixel);
 			write_color_ppm(pixel_color, samples_per_pixel, outputData, img);
 		}
 	}
 
 	img.write("test.ppm", outputData);
-
 	std::cerr << "\nDone.\n";
 }
 
