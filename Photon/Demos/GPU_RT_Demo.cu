61aa07f9ae388f2450b3dc47bb4ae36f9c41e5a5
PC88 <32823980+PC88@users.noreply.github.com>
2021-04-29 19:40:03 +0100

    first version converted to PPM and output correctly. more to come rapidly now.

--- Photon/Demos/GPU_RT_Demo.cu
+++ Photon/Demos/GPU_RT_Demo.cu
@@ -21,6 +21,13 @@ void check_cuda(cudaError_t result, char const* const func, const char* const fi
 	}
 }
 
+__device__ vec3 colour(const ray& r) 
+{
+	vec3 unit_direction = unit_vector(r.direction());
+	float t = 0.5f * (unit_direction.y() + 1.0f);
+	return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
 __global__ void render(vec3* fb, int max_x, int max_y)
 {
 	int i = threadIdx.x + blockIdx.x * blockDim.x;
@@ -59,6 +66,17 @@ GPU_RT_Demo::GPU_RT_Demo()
 	double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
 	std::cerr << "took " << timer_seconds << " seconds.\n";
 
+
+	ppm img;
+	img.w = nx;
+	img.h = ny;
+	img.magic = "P3";
+	img.max = 255;
+	img.capacity = img.w * img.h * img.nchannels;
+	std::vector<unsigned char> outputData;
+
+
+	std::vector<unsigned char> data;
 	// Output FB as Image
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 	for (int j = ny - 1; j >= 0; j--) 
@@ -69,9 +87,18 @@ GPU_RT_Demo::GPU_RT_Demo()
 			int ir = int(255.99 * fb[pixel_index].r());
 			int ig = int(255.99 * fb[pixel_index].g());
 			int ib = int(255.99 * fb[pixel_index].b());
-			std::cout << ir << " " << ig << " " << ib << "\n";
+			//std::cout << ir << " " << ig << " " << ib << "\n";
+
+			// Write the translated [0,255] value of each color component.
+
+			//data.resize(img.w * img.h * img.nchannels);
+			data.push_back(static_cast<int>(ir));
+			data.push_back(static_cast<int>(ig));
+			data.push_back(static_cast<int>(ib));
 		}
 	}
+
+	img.write("gpu_test.ppm", data);
 
 	checkCudaErrors(cudaFree(fb));
 }
